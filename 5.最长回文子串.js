2a07af542c6ece6bad9ffc0960b3fae32dd5384d
qinchenguang <943814194@qq.com>
2019-07-01 16:36:32 +0800

    feat: 解第五题 同时要注意处理极值和边界情况

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -29,11 +29,68 @@
  * 
  * 
  */
+function handleGetnumber(left,right,s,length){
+  if(s[left] !== s[right]){
+    return [left, 1];
+  } else {
+    if(left > 0 && right < length - 1) {
+      left --;
+      right ++;
+    }
+  }
+  // console.log(left,right)
+  while(s[left] !== undefined && s[right] !== undefined) {
+    if(s[left] !== s[right]){
+      left ++;
+      right --;
+      break;
+    } else {
+      if(left > 0 && right < length - 1) {
+        left --;
+        right ++;
+      } else {
+        break;
+      }
+    }
+  }
+  return [left > 0 ? left : 0, right - Math.abs(left) + 1];
+}
 /**
  * @param {string} s
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    
+  let length = s.length;
+  if(s.length <= 1) {
+    return s;
+  }
+  let str = '';
+  for(let i = 0; i < s.length; i++) {
+    let left,right;
+    let array = handleGetnumber(i, i + 1, s, length);
+    let array2 = handleGetnumber(i,i + 2, s, length);
+    if(array2[1] > array[1]){
+      left = array2[0];
+      right = array2[0] + array2[1];
+    } else {
+      left = array[0];
+      right = array[0] + array[1];
+    }
+    // console.log(array2, array)
+    let targetStr = s.slice(left, right);
+    str = targetStr.length >= str.length ? targetStr : str;
+  }
+  // console.log(str ? str : s[0] || s);
+
+  return str ? str : s[0] || s;
 };
 
+// longestPalindrome('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+// longestPalindrome('ccc');
+// longestPalindrome('ccc');
+// longestPalindrome('aba');
+// 回文串从左到右和从右到左都是一致
+// 从左到右进行检查
+// 偶数位是左右对称
+// 奇数位是关于中间对称
+// 对比方式 从第一位依次往后2位对比
