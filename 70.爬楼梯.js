4329506cc916c251d3dfeecb953903a669604cb5
qinchenguang <943814194@qq.com>
2019-07-04 10:11:33 +0800

    feat: 比较考验思路和数学

--- 70.爬楼梯.js
+++ 70.爬楼梯.js
@@ -43,6 +43,10 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    
+  var arr = [0, 1, 2];
+  for(let i = 3; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n]
 };
-
+// 规律比较难找
