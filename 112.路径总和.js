8227024dc689a9e72ecc482709db7b4a8cfe8d6b
qinchenguang <943814194@qq.com>
2019-07-08 09:49:59 +0800

    feat: beautiful
    根据条件找到可以递归的方法和递归的临界条件

---
+++ 112.路径总和.js
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=javascript
+ *
+ * [112] 路径总和
+ *
+ * https://leetcode-cn.com/problems/path-sum/description/
+ *
+ * algorithms
+ * Easy (46.82%)
+ * Likes:    150
+ * Dislikes: 0
+ * Total Accepted:    21.3K
+ * Total Submissions: 45.3K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,null,1]\n22'
+ *
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例: 
+ * 给定如下二叉树，以及目标和 sum = 22，
+ * 
+ * ⁠             5
+ * ⁠            / \
+ * ⁠           4   8
+ * ⁠          /   / \
+ * ⁠         11  13  4
+ * ⁠        /  \      \
+ * ⁠       7    2      1
+ * 
+ * 
+ * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+  if(!root) return false;
+  // 值可能为负数
+  // if(root.val > sum) return false;
+
+  // 判断是否是叶子节点
+  if(!root.left && !root.right) {
+    return sum === root.val;
+  }
+  return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+};
+// 路径和有两种计算方法，从根元素开始，判断路径上面的和是否等于sum 
+// 另外一种从叶节点开始递归。 
+// 这里采用跟节点计算。
